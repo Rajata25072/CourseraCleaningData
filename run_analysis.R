b243470c668b7dac699fa5285f507f3ad5fc73a2
lachrymator <jwarr885@gmail.com>
2014-05-26 10:12:52 -0700

    Minor Typo Fixes

--- run_analysis.R
+++ run_analysis.R
@@ -9,7 +9,7 @@
 # activitylabels - The names and associated levels of all the measured activities
 
 
-#initialize libraries if not found, attempt to load and install
+#initialize libraries. If not found, attempt to load and install
 
 if(require(reshape2)) {
   message("Library reshape2 loaded correctly")
@@ -22,7 +22,6 @@ if(require(reshape2)) {
     stop("Could not install reshape2")
   }
 }
-
 
 if(require(plyr)) {
   message("Library plyr loaded correctly")
@@ -48,6 +47,7 @@ acts <- "UCI HAR Dataset/activity_labels.txt"
 trainsubs <-"UCI HAR Dataset/train/subject_train.txt"
 testsubs <- "UCI HAR Dataset/test/subject_test.txt"
 
+
 ## Reassembling training and test data from files for X, Y, and Subjects
 
 message("Reading data...")
@@ -55,6 +55,7 @@ xdata <-rbind(read.table(xtrain),read.table(xtest))
 ydata <-rbind(read.table(ytrain),read.table(ytest))
 subjects<-rbind(read.table(trainsubs),read.table(testsubs))
 message("Complete.")
+
 
 ## Assigning proper feature names and subject names
 
@@ -69,34 +70,40 @@ colsubset<-c(grep("mean\\(", featnames[,2]), grep("std\\(", featnames[,2]))
 colsubset<-sort(colsubset)
 xdata<-xdata[,colsubset]
 
-##Assigning proper Activity names
+
+## Assigning proper Activity names
 
 activitylabels<-read.table(acts)
 activitylabels[,2]<-tolower(activitylabels[,2])
 ydata<-join(ydata, activitylabels, by = "V1")
 names(ydata)<-c("id","activity")
 
-#combining data frames of first subjects, then activities, and finally xdata
+
+# combining data frames of first subjects, then activities, and finally xdata
 
 df <- cbind(subjects,ydata["activity"],xdata)
 
-#clearing temporary dataframes
+
+# clearing temporary dataframes
 
 rm(subjects, featnames, xdata, ydata)
 
-#formatting and cleaning data frame names
+
+# formatting and cleaning data frame names
 
 names(df) <- gsub("-", ".", names(df))
 names(df) <- sub("mean\\(\\)", "mean", names(df))
 names(df) <- sub("std\\(\\)", "std", names(df))
 names(df) <- sub("BodyBody", "Body", names(df))
 
-#melting data frame by activity and subject, and recasting by mean
+
+# melting data frame by activity and subject, and recasting by mean
 
 dfmelted <- melt(df, id=c("activity","subject"),measure.vars=names(df)[3:length(names(df))])
 dfcasted <- dcast(dfmelted, activity + subject ~ variable, mean)
 
-#write finished analysis
+
+# write finished analysis
 
 message("")
 message("Writing solution to JWCourseProjectSoln.txt ....")
