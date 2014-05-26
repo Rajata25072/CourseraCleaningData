b514a8aa114fecdfcf20ffdb49c5c61edbe17e80
lachrymator <jwarr885@gmail.com>
2014-05-26 10:07:59 -0700

    Minor Typo Fixes

--- run_analysis.R
+++ run_analysis.R
@@ -10,6 +10,20 @@
 
 
 #initialize libraries if not found, attempt to load and install
+
+if(require(reshape2)) {
+  message("Library reshape2 loaded correctly")
+} else {
+  message("Library reshape2 not found, attempting to install...")
+  install.packages("reshape2")
+  if(require(plyr)) {
+    message("Library reshape2 installed and loaded correctly")
+  } else {
+    stop("Could not install reshape2")
+  }
+}
+
+
 if(require(plyr)) {
   message("Library plyr loaded correctly")
 } else {
@@ -22,19 +36,9 @@ if(require(plyr)) {
   }
 }
 
-if(require(reshape2)) {
-  message("Library reshape2 loaded correctly")
-} else {
-  message("Library reshape2 not found, attempting to install...")
-  install.packages("plyr")
-  if(require(plyr)) {
-    message("LIbrary reshape2 installed and loaded correctly")
-  } else {
-    stop("Could not install reshape2")
-  }
-}
 
 ## File Locations
+
 xtrain <- "UCI HAR Dataset/train/X_train.txt"
 ytrain <- "UCI HAR Dataset/train/Y_train.txt"
 xtest <- "UCI HAR Dataset/test/X_test.txt"
@@ -45,6 +49,7 @@ trainsubs <-"UCI HAR Dataset/train/subject_train.txt"
 testsubs <- "UCI HAR Dataset/test/subject_test.txt"
 
 ## Reassembling training and test data from files for X, Y, and Subjects
+
 message("Reading data...")
 xdata <-rbind(read.table(xtrain),read.table(xtest))
 ydata <-rbind(read.table(ytrain),read.table(ytest))
@@ -52,39 +57,47 @@ subjects<-rbind(read.table(trainsubs),read.table(testsubs))
 message("Complete.")
 
 ## Assigning proper feature names and subject names
+
 featnames<-read.table(feats)
 names(xdata)<-featnames[,2]
 names(subjects) <- "subject"
 
 
 # trim xdata to only have means and standard deviations
+
 colsubset<-c(grep("mean\\(", featnames[,2]), grep("std\\(", featnames[,2]))
 colsubset<-sort(colsubset)
 xdata<-xdata[,colsubset]
 
 ##Assigning proper Activity names
+
 activitylabels<-read.table(acts)
 activitylabels[,2]<-tolower(activitylabels[,2])
 ydata<-join(ydata, activitylabels, by = "V1")
 names(ydata)<-c("id","activity")
 
 #combining data frames of first subjects, then activities, and finally xdata
+
 df <- cbind(subjects,ydata["activity"],xdata)
 
 #clearing temporary dataframes
+
 rm(subjects, featnames, xdata, ydata)
 
 #formatting and cleaning data frame names
+
 names(df) <- gsub("-", ".", names(df))
 names(df) <- sub("mean\\(\\)", "mean", names(df))
 names(df) <- sub("std\\(\\)", "std", names(df))
 names(df) <- sub("BodyBody", "Body", names(df))
 
 #melting data frame by activity and subject, and recasting by mean
+
 dfmelted <- melt(df, id=c("activity","subject"),measure.vars=names(df)[3:length(names(df))])
 dfcasted <- dcast(dfmelted, activity + subject ~ variable, mean)
 
 #write finished analysis
+
 message("")
 message("Writing solution to JWCourseProjectSoln.txt ....")
 write.table(dfcasted, file = "JWCourseProjectSoln.txt")
